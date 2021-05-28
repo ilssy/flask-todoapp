04cad7088ac0bd09a2470f02f6001cc20ce05df0
Ilze <ilze.kapeniece@gmail.com>
2021-05-28 20:42:51 +0300

    add category column

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -22,7 +23,8 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        task_category = request.form['category']
+        new_task = Todo(content=task_content, category=task_category)
 
         try:
             db.session.add(new_task)
@@ -34,7 +36,6 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
-    # return render_template('index.html')
 
 @app.route('/delete/<int:id>')
 def delete(id):
